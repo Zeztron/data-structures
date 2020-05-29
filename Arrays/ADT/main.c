0cdce74d5f2c495410cbe48fd770537fbfc7eccd
Zeztron <pateltharsh@gmail.com>
2020-05-28 20:16:54 -0400

    added binary search

--- Arrays/ADT/main.c
+++ Arrays/ADT/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 struct Array {
-  int A;
+  int A[10];
   int size;
   int length;
 };
@@ -24,16 +24,65 @@ void Append(struct Array *arr, int x) {
 
 // Inserts element in a specific index
 void Insert(struct Array *arr, int index, int x) {
-  
   int i;
-  if (index>=0 && arr->length) {
-    for(i=arr->length, i>index; i--) {
+  if (index>=0 && index<=arr->length) {
+    for(i=arr->length; i>index; i--) {
       arr->A[i] = arr->A[i-1];
     }
 
     arr->A[index] = x;
-    arr->length++
+    arr->length++;
   }
+}
+
+int BinarySearch(struct Array arr, int key) {
+  int low = 0;
+  int high = arr.length-1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if(key == arr.A[mid]) {
+      return mid;
+    } else if (key < arr.A[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  
+  return -1;
+}
+
+int RecursiveBinarySearch(int arr[], int low, int high, int key) {
+
+  if (low <= high) {
+
+  int mid = (low + high) / 2;
+    if (key == arr[mid]) {
+      return mid;
+    } else if (key < arr[mid]) {
+      return RecursiveBinarySearch(arr, low, mid-1, key);
+    } else {
+      return RecursiveBinarySearch(arr, mid + 1, high, key);
+    }
+  }
+
+  return -1;
+}
+
+int Delete(struct Array *arr, int index) {
+  int x=0;
+  int i;
+  if (index>=0 && index<=arr->length-1) {
+    x=arr->A[index];
+    for (i=index; i<arr->length-1; i++) {
+      arr->A[i] = arr->A[i + 1];
+    }
+    arr->length--;
+    return x;
+  }
+
+  return 0;
 }
 
 int main() {
@@ -41,6 +90,7 @@ int main() {
 
   Append(&arr, 10);
   Insert(&arr, 5, 10);
+  Delete(&arr, 4);
   Display(arr);
   return 0;
 }
