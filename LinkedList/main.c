72c1c275102343117d0f705c05a54f46e8d5e912
Zeztron <pateltharsh@gmail.com>
2020-06-02 11:49:16 -0400

    added sum and count

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,39 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Node {
+struct Node 
+{
   int data;
   struct Node *next;
 }*first;
 
-void create(int A[], int n) {
+void create(int A[], int n) 
+{
   int i;
   struct Node *t, *last;
 
   first = (struct Node*)malloc(sizeof(struct Node));
-  first -> data = A[0];
-  first -> next = NULL;
+  first->data = A[0];
+  first->next = NULL;
   last = first;
 
   for (i = 1; i < n; i++) {
     t=(struct Node*)malloc(sizeof(struct Node));
-    t -> data = A[i];
-    t -> next = NULL;
-    last -> next = t;
+    t->data = A[i];
+    t->next = NULL;
+    last->next = t;
   }
 }
 
-void Display(struct Node *p) {
+void Display(struct Node *p) 
+{
   while (p != NULL) {
-    printf("%d", p -> data);
-    p = p -> next;
+    printf("%d", p->data);
+    p = p->next;
   }
 }
 
-int main() {
+int CountNodes(struct Node *p) 
+{
+  int count = 0;
+  while (p != NULL) {
+    count++;
+    p = p->next;
+  }
+
+  return count;
+}
+
+int RecursiveCount(struct Node *p) 
+{
+  if (p == 0) {
+    return 0;
+  } else {
+    return RecursiveCount(p->next) + 1;
+  }
+}
+
+int Sum(struct Node *p)
+{
+  int sum = 0;
+  while (p != NULL) {
+    sum += p->data;
+    p = p->next;
+  }
+
+  return sum;
+}
+
+int RecursiveSum(struct Node *p) 
+{
+  if (p == 0) {
+    return 0;
+  } else {
+    return RecursiveSum(p->next) + p->data;
+  }
+}
+
+int main() 
+{
   int A[] = {3, 5, 7, 10, 15};
   create(A, 5);
 
   Display(first);
+  CountNodes(first);
+  sum(first);
 
   return 0;
 }
