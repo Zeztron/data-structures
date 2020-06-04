719bcad573ac43a9d3a9f123e629532ca2f166f7
Zeztron <pateltharsh@gmail.com>
2020-06-04 17:42:50 -0400

    added insert

--- CircularLinkedList/main.c
+++ CircularLinkedList/main.c
@@ -47,6 +47,34 @@ void RecursiveDisplay(struct Node *h)
   flag = 0;
 }
 
+void Insert(struct Node *p, int index, int x)
+{
+  struct Node *t;
+  int i;
+  t = (struct Node *)malloc(sizeof(struct Node));
+  t->data = x;
+  if (index == 0) {
+    if (Head == NULL) {
+      Head = t;
+      Head->next = Head;
+    } else {
+      p = Head;
+      while (p->next != Head) {
+        p = p->next;
+      }
+      p->next = t;
+      Head = t;
+    }
+  } else {
+    for (i - 0; i < index - 1; i++) {
+      p = p->next;
+    }
+
+    t->next = p->next;
+    p->next = t;
+  }
+}
+
 int main()
 {
   int A[] = {2, 3, 4, 5, 6};
