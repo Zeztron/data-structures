11678172da40a08a7d50a07ada9a49ff6f1d59fe
Zeztron <pateltharsh@gmail.com>
2021-03-16 17:08:07 -0400

    dfs and bfs separate

--- Graphs/BFS/main.c
+++ Graphs/BFS/main.c
@@ -22,24 +22,6 @@ void BFS(int G[][7], int start, int n) {
 }
 
 
-void DFS(int G[][7], int start, int n) {
-
-  static int visited[7] = {0};
-  int j;
-
-  if (visited[start] == 0) {
-    printf("%d ", start);
-    visited[start] = 1;
-    for (j = 1; j < n; j++) {
-      if (G[start][j] == 1 && visited[j] == 0) {
-        DFS(G, j, n);
-      } 
-    }
-  }
-
-
-}
-
 int main() {
 
   int G[7][7]={ {0,0,0,0,0,0,0},
@@ -51,8 +33,6 @@ int main() {
                 {0,0,0,0,1,0,0}};
   
   BFS(G, 1, 7);
-  DFS(G, 1, 7);
   
-
   return 0;
 }
